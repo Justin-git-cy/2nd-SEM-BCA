dd1a859180073c44f07e0f242594f6749467da23
Justin-git-cy <justinmj2006@gmail.com>
2025-02-05 14:39:22 +0530

    method.py

---
+++ CS1280-PPDA/Lab3/method.py
@@ -0,0 +1,8 @@
+num = "123"
+name = "asia is a continent"
+print(name.isupper()) # False
+print(name.islower()) # True
+print(name.isalnum()) # False
+print(name.isalpha()) # False
+print(name.split()) # ['asia', 'is', 'a', 'continent']
+print(num.isdecimal()) # True
