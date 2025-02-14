e040125b89d1ea6d24e3b32e9a3c8be52932fe3e
Justin-git-cy <justinmj2006@gmail.com>
2025-02-14 10:32:40 +0530

    Create dictionary.py

---
+++ CS1280-PPDA/Lab4/dictionary.py
@@ -0,0 +1,13 @@
+BCA= {"1":"Ram",
+      "2":"ravi",
+      "3":"my"}
+
+#calling key values
+print("Here are the keys")
+  for key in BCA:
+      print(key)
+      
+#calling values values
+print("Here are the values")
+for key in BCA:
+     print(BCA[key])
