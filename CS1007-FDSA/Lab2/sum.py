bb2bcc6072f7848c3c2890c4437b15ce38a1246c
Justin-git-cy <justinmj2006@gmail.com>
2025-02-19 10:21:17 +0530

    Create sum.py

---
+++ CS1007-FDSA/Lab2/sum.py
@@ -0,0 +1,10 @@
+def sum_of_digits(n):
+    if n == 0:
+        return 0
+    
+    return n % 10 + sum_of_digits(n // 10)
+
+number = 12345
+result = sum_of_digits(number)
+print(f"The sum of digits of {number} is {result}")
+
