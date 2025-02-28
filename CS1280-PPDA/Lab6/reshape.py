10fa4193589eb1f1238d82feb92e53143064c010
Justin-git-cy <justinmj2006@gmail.com>
2025-02-28 10:47:04 +0530

    Add files via upload

---
+++ CS1280-PPDA/Lab6/reshape.py
@@ -0,0 +1,9 @@
+import numpt as np
+arr = np.arange(24) 
+
+reshaped_arr = arr.reshape(6, 4)
+print("Original Array (1D):\n", arr)
+print("Reshaped Array (3x4):\n", reshaped_arr)
+
+reshaped_arr_3D = arr.reshape(2, 3, 4)
+print("Reshaped Array (2x3x4):\n", reshaped_arr_3D)
