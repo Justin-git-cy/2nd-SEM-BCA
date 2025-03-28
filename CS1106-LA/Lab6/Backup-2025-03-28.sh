1d29ad6e4a5ad3059dba75be72f8a865a6fe2c73
Justin-git-cy <justinmj2006@gmail.com>
2025-03-28 09:47:11 +0530

    Create Backup-2025-03-28.sh

---
+++ CS1106-LA/Lab6/Backup-2025-03-28.sh
@@ -0,0 +1,8 @@
+mkdir -p /temp/backup
+cp *.c *.py /temp/backup
+cd /temp/
+tar -cvzf /temp/backup.tar.gz -C /temp backup
+udisksctl mount -b /dev/sda1
+mv /temp/backup.tar.gz /media/rvu/pendrive/
+udisksctl umount -b /dev/sda1
+rmdir /temp/backup
