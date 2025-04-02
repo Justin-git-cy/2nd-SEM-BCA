fe9d477627fa9f9932fae7fbb629cec7a4db4254
Justin-git-cy <justinmj2006@gmail.com>
2025-04-02 17:11:39 +0530

    Create 5A.py

---
+++ CS1280-PPDA/Lab_Activities/5A.py
@@ -0,0 +1,5 @@
+def matrix_operations():
+    """Performs matrix operations using NumPy."""
+    mat1 = np.array([[1, 2], [3, 4]])
+    mat2 = np.array([[5, 6], [7, 8]])
+    return np.dot(mat1, mat2)  # Matrix multiplication
