5e1a186c77db00be0657d38d6c8254e7116aaba6
Justin-git-cy <justinmj2006@gmail.com>
2025-04-03 17:01:31 +0530

    Update 3B.py

--- CS1280-PPDA/Lab_Activities/3B.py
+++ CS1280-PPDA/Lab_Activities/3B.py
@@ -1,6 +1,6 @@
 def merge_dictionaries(dict1, dict2):
-    merged_dict = dict1.copy()  # Copy first dictionary to avoid modifying the original
-    merged_dict.update(dict2)   # Merge with second dictionary
+    merged_dict = dict1.copy()  
+    merged_dict.update(dict2)   
     return merged_dict
 
 dict1 = {"a": 1, "b": 2, "c": 3}
