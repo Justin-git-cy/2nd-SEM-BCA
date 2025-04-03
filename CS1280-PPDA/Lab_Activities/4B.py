f50fdb41615ed98e3c928371231e5316af69cdc8
Justin-git-cy <justinmj2006@gmail.com>
2025-04-03 16:59:46 +0530

    Update 4B.py

--- CS1280-PPDA/Lab_Activities/4B.py
+++ CS1280-PPDA/Lab_Activities/4B.py
@@ -1,12 +1,13 @@
 from google.colab import drive
+import os
+import shutil  
+
 drive.mount('/content/drive')
 
-# List files in a Google Drive folder
-import os
 path = "/content/drive/My Drive/"
+
 files = os.listdir(path)
 print("Files in Google Drive:", files)
 
-# Copying a file in Google Drive
 shutil.copy(path + "file1.txt", path + "file1_copy.txt")
 print("File copied in Google Drive")
