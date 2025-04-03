f9d6b2c68d90fad3ac9d19509ff67ce599b176d7
Justin-git-cy <justinmj2006@gmail.com>
2025-04-03 15:22:21 +0530

    Update 12B.py

--- CS1280-PPDA/Lab_Activities/12B.py
+++ CS1280-PPDA/Lab_Activities/12B.py
@@ -1,5 +1,9 @@
-def time_series_analysis(df, date_column, value_column):
-    """Performs time series analysis on stock prices."""
-    df[date_column] = pd.to_datetime(df[date_column])
-    df.set_index(date_column, inplace=True)
-    return df[value_column].resample('M').mean()  
+Q1 = df["Values"].quantile(0.25)  
+Q3 = df["Values"].quantile(0.75)  
+IQR = Q3 - Q1 
+
+lower_bound = Q1 - 1.5 * IQR
+upper_bound = Q3 + 1.5 * IQR
+
+outliers = df[(df["Values"] < lower_bound) | (df["Values"] > upper_bound)]
+print("\nOutliers detected:\n", outliers)
