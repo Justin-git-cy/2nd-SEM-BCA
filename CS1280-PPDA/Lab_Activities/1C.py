744e09171be9e5052ec8597e78471052d22ff0cb
Justin-git-cy <justinmj2006@gmail.com>
2025-04-03 14:47:01 +0530

    Update 1C.py

--- CS1280-PPDA/Lab_Activities/1C.py
+++ CS1280-PPDA/Lab_Activities/1C.py
@@ -1,3 +1,11 @@
-def largest_of_three(a, b, c):
-    """Finds the largest number among three."""
-    return max(a, b, c)
+def find_largest():
+    print("Find the Largest Number")
+    num1 = float(input("Enter first number: "))
+    num2 = float(input("Enter second number: "))
+    num3 = float(input("Enter third number: "))
+
+    largest = max(num1, num2, num3)
+
+    print("The largest number is:", largest)
+
+find_largest()
