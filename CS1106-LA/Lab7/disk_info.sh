734f87a2b76178c2ba93058e3af138b3f84194e6
Justin-git-cy <justinmj2006@gmail.com>
2025-04-03 10:21:01 +0530

    Update disk_info.sh

--- CS1106-LA/Lab7/disk_info.sh
+++ CS1106-LA/Lab7/disk_info.sh
@@ -1,7 +1,3 @@
 #!/bin/bash
 
-# Use udisksctl to list mounted filesystems
 udisksctl status | grep -E "Mount Point" | awk '{print $2}'
-
-# Alternatively, use udisksctl info to get more details
-# udisksctl info --block-device /dev/sdXN | grep "Mount Point"
