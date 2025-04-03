191047730ec3323d870b5e7257b9f3c79b8d60e8
Justin-git-cy <justinmj2006@gmail.com>
2025-04-03 15:26:20 +0530

    Update 15A.py

--- CS1280-PPDA/Lab_Activities/15A.py
+++ CS1280-PPDA/Lab_Activities/15A.py
@@ -1,14 +1,9 @@
-import seaborn as sns
-import matplotlib.pyplot as plt
+df = pd.DataFrame(np.random.rand(10, 4), columns=["A", "B", "C", "D"])
 
-# Load sample dataset
-penguins = sns.load_dataset("penguins")
+corr_matrix = df.corr()
 
-# Compute correlation matrix (dropping non-numeric columns)
-corr_matrix = penguins.drop(columns=["species", "island", "sex"]).corr()
-
-# Plot heatmap
+# **Plot Heatmap**
 plt.figure(figsize=(8, 6))
-sns.heatmap(corr_matrix, annot=True, cmap="coolwarm", fmt=".2f", linewidths=0.5)
+sns.heatmap(corr_matrix, annot=True, cmap="coolwarm", linewidths=0.5)
 plt.title("Correlation Matrix Heatmap")
 plt.show()
