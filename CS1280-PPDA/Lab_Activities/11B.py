c7cebef0f8ec5dacb01f5c43313bd00bfff8183c
Justin-git-cy <justinmj2006@gmail.com>
2025-04-03 15:21:11 +0530

    Delete CS1280-PPDA/Lab_Activities/11B.py

--- CS1280-PPDA/Lab_Activities/11B.py
+++
@@ -1,9 +0,0 @@
-def identify_outliers(df, column):
-    """Identifies outliers using the IQR method and returns a DataFrame with outlier information."""
-    Q1 = df[column].quantile(0.25)
-    Q3 = df[column].quantile(0.75)
-    IQR = Q3 - Q1
-    lower_bound = Q1 - 1.5 * IQR
-    upper_bound = Q3 + 1.5 * IQR
-    outliers = df[(df[column] < lower_bound) | (df[column] > upper_bound)]
-    return outliers, lower_bound, upper_bound
