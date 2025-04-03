19e88591b18a305b33a0074e90dd2f9c4d6ca676
Justin-git-cy <justinmj2006@gmail.com>
2025-04-03 19:12:42 +0530

    Create commands.sh

---
+++ CS1106-LA/Practice_questions/Set3/commands.sh
@@ -0,0 +1,7 @@
+head -n 5 /etc/passwd
+mkdir -p data/2025/{jan,feb,mar}
+find /etc -type f -name "*.conf" | wc -l
+grep "permission denied" /var/log/auth.log
+dd if=/dev/zero of=testfile bs=1M count=100
+free -h
+mount
