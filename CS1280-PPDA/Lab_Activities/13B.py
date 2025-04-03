f05f38634e9d266fa00de74e242e953b9592dad3
Justin-git-cy <justinmj2006@gmail.com>
2025-04-03 15:24:23 +0530

    Update 13B.py

--- CS1280-PPDA/Lab_Activities/13B.py
+++ CS1280-PPDA/Lab_Activities/13B.py
@@ -1,5 +1,25 @@
-import numpy as np
+import pandas as pd
+import matplotlib.pyplot as plt
 
-dates = pd.date_range('20240101', periods=10)
-stocks = pd.DataFrame({'Date': dates, 'Price': np.random.rand(10) * 100})
-stocks.set_index('Date', inplace=True)
+df = pd.read_csv("stock_prices.csv", parse_dates=["Date"], index_col="Date")
+
+plt.figure(figsize=(10, 5))
+plt.plot(df.index, df["Close"], label="Stock Price", color='b')
+plt.xlabel("Date")
+plt.ylabel("Stock Price")
+plt.title("Stock Price Trend Over Time")
+plt.legend()
+plt.show()
+
+df["50-day MA"] = df["Close"].rolling(window=50).mean()
+df["200-day MA"] = df["Close"].rolling(window=200).mean()
+
+plt.figure(figsize=(10, 5))
+plt.plot(df.index, df["Close"], label="Stock Price", color='b')
+plt.plot(df.index, df["50-day MA"], label="50-Day MA", color='r')
+plt.plot(df.index, df["200-day MA"], label="200-Day MA", color='g')
+plt.xlabel("Date")
+plt.ylabel("Stock Price")
+plt.title("Stock Price with Moving Averages")
+plt.legend()
+plt.show()
